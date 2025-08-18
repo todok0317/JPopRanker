78ada2d5cfecaae1901d549bfaed419a00421b9b
todok0317 <todok0317@naver.com>
2025-08-18 13:32:28 +0900

    Initial commit

---
+++ src/main/java/com/example/jpopranker/JPopRankerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpopranker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPopRankerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JPopRankerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpopranker/JPopRankerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpopranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPopRankerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
