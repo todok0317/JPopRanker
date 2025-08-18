80470c4ae974f69fc1f61d5352707dbda02c19db
todok0317 <todok0317@naver.com>
2025-08-18 14:05:12 +0900

    feat : entity 설정

---
+++ src/main/java/com/example/jpopranker/entity/Song.java
@@ -0,0 +1,48 @@
+package com.example.jpopranker.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "songs")
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String artist;
+
+    private Integer ranking;
+
+    @Column(name = "chart_name")
+    private String chartName;
+
+    @Column(name = "chart_date")
+    private LocalDateTime chartDate;
+
+    @Column(name = "created_at")
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+
+
+}
